220f94128af2fa5144ece0f300817b39d8821daa
yazid <flight846@gmail.com>
2016-07-25 15:54:53 +0800

    Add index.ejs

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+// const io = ('socket.io')(server)
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+io.on('connection', function (socket) {
+  console.log('a user connected')
+  socket.on('disconnect', function () { // when a browser is closed, a user is disconnected
+    console.log('user disconnected')
+  })
+})
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg)
+  })
+})
 
 server.listen(3000, () => {
   console.log('server listening on port 3000')
